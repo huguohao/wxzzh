a48dfd577b8d4fea8af7f489c1f2ff4d1b426738
unknown <>
2017-06-15 20:06:44 +0800

    receive.py fix

--- receive.py
+++ receive.py
@@ -1,10 +1,9 @@
-#-*-coding:utf-8-*-
-#filename:receive.py
-
+# -*- coding: utf-8 -*-
+# filename: receive.py
 import xml.etree.ElementTree as ET
 
 def parse_xml(web_data):
-    if len(web_data)== 0:
+    if len(web_data) == 0:
         return None
     xmlData = ET.fromstring(web_data)
     msg_type = xmlData.find('MsgType').text
@@ -12,20 +11,22 @@ def parse_xml(web_data):
         return TextMsg(xmlData)
     elif msg_type == 'image':
         return ImageMsg(xmlData)
+
 class Msg(object):
-    def __init__(self,xmlData):
+    def __init__(self, xmlData):
         self.ToUserName = xmlData.find('ToUserName').text
         self.FromUserName = xmlData.find('FromUserName').text
-        self.CreateTime = xmlData.fing('CreateTime').text
+        self.CreateTime = xmlData.find('CreateTime').text
         self.MsgType = xmlData.find('MsgType').text
         self.MsgId = xmlData.find('MsgId').text
+
 class TextMsg(Msg):
-    def __init__(self,xmlData):
-        Msg.__init__(self,xmlData)
+    def __init__(self, xmlData):
+        Msg.__init__(self, xmlData)
         self.Content = xmlData.find('Content').text.encode("utf-8")
 
 class ImageMsg(Msg):
-    def __init__(self,xmlData):
-        Msg.__init__(self,xmlData)
-        self.PirUrl = xmlData.find('PirUrl').text
+    def __init__(self, xmlData):
+        Msg.__init__(self, xmlData)
+        self.PicUrl = xmlData.find('PicUrl').text
         self.MediaId = xmlData.find('MediaId').text
